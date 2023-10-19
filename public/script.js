683ceeaf56ce03ccf5914110ee8a0911bda00f2d
dhanvantichelani <dhanvantichelani865@gmail.com>
2023-10-19 21:22:24 +0530

    first-commit

---
+++ public/script.js
@@ -0,0 +1,43 @@
+let username = "";
+document.getElementById('join-btn').addEventListener('click',(event)=>{
+  event.preventDefault(); //for not refreshing webpage
+  username = document.getElementById("username-input").value;
+//console.log('test');
+if(username.trim()!=""){
+  document.querySelector(".form-username").style.display='none';
+  document.querySelector(".chatroom-container").style.display='block';
+}
+})
+
+document.getElementById('send-btn').addEventListener('click',(event)=>{
+  event.preventDefault();
+  const data = {
+      username: username,
+      message: (document.getElementById('message-input').value).trim(),
+  }
+  // emiting with 'message' event
+  socket.emit('message',data);
+  addMessage(data,true); // true -> sent
+})
+
+// recieving the message
+socket.on('message',(data)=>{
+  if(data.username !==username){
+      addMessage(data,false);
+  }
+})
+
+// this function is just for appending message
+function addMessage(data, check){
+  // check -> true for sent
+  // check -> false for recieved
+  var msgDiv = document.createElement('div');
+  msgDiv.innerText = `${data.username}: ${data.message}`;
+  if(check){
+      msgDiv.setAttribute('class', 'message sent');
+  } else{
+      msgDiv.setAttribute('class', 'message recieved');
+  }
+  document.getElementById('message-container').appendChild(msgDiv);
+  document.getElementById('message-input').value="";
+}
